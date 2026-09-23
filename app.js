6fbaacf90548b59d0012407860c98aac4093b1ea
angela! <angela12moya@gmail.com>
2026-09-23 11:42:58 +0200

    js arreglado

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 alert("online")
 console.log("Este es un mensaje normal en la consola");
 // TIPOS DE VARIABLES (cajas q elijen un valor)
-var userame =Adrian; //texto
+var userame = "Adrian"; //texto
 var uage = 37; //numero entero - interger
 var ulicense = true;  //boolean - true o false
 var colors = ["red", "blue" , "green"]//array o lista
+console.log(colors);
+console.log(colors[1]);
+
 var values = [21, 45, 67, 26]
 //LLAMAR A AUNA VARIABLE SOLO PONER NOMBRE
 username = "Pepito";
-console.log(username);
+console.log (username);
 var total_price;
 var price01 = 23;
 var price02 = 42;
 total_price = price01 + price02;
-console.log = "total_price"
+console.log (total_price);
 //operaciones
 //mayor >
 //menor <
-// igual ===
+// igual ==
 // exactamente igual ===
 // console.log(10 > 5);
 // En el console saldrá true, porque le estamos diciendo que 10 es mayor que 5 y eso es verdadero. En este caso solo acepta true or false. Vale por ej para las páginas de bebidas alcoholicas q te preguntan si eres mayor de 18 (metiendo el dato de tu edad)
-console.log (10 === "10");
+console.log(10===10);
 
 //concatenacion
 
 var name2 = "Manolito";
-var greeting = "Hola" + Name2
+var greeting = "Hola " + name2;
+console.log (greeting);
